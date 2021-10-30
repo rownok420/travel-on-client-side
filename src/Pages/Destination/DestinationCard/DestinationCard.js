9cc267f58c736a567b415682fc8aea826732e411
rownok420 <mrbluebird420@gmail.com>
2021-10-30 11:38:10 +0600

    get destination data dynamically

--- src/Pages/Destination/DestinationCard/DestinationCard.js
+++ src/Pages/Destination/DestinationCard/DestinationCard.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
-import img1 from '../../../Images/place/place-1.jpg'
-import img2 from '../../../Images/place/place-2.jpg'
-import img3 from '../../../Images/place/place-3.jpg'
-import img4 from '../../../Images/place/place-4.jpg'
-import img5 from '../../../Images/place/place-5.jpg'
-import img6 from '../../../Images/place/place-6.jpg'
-import img7 from '../../../Images/place/place-7.jpg'
-import img8 from '../../../Images/place/place-8.jpg'
-import img9 from '../../../Images/place/place-9.jpg'
-import './DestinationCard.css'
+import "./DestinationCard.css";
 
 const DestinationCard = () => {
+    const [destinations, setDestinations] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/destination")
+            .then((res) => res.json())
+            .then((data) => {
+                setDestinations(data);
+            });
+    }, []);
+
     return (
         <div className="my-5">
             <Container>
@@ -23,116 +23,26 @@ const DestinationCard = () => {
                         visitors to easily book their next tour.
                     </p>
                 </div>
-                <div className='mt-5'>
+                <div className="mt-5">
                     <Row xs={1} md={2} lg={3} className="g-4">
-                        <Col>
-                            <Card className="h-100 card-style">
-                                <div className="img-container">
-                                    <Card.Img
-                                        className="p-3 card-img"
-                                        variant="top"
-                                        src={img1}
-                                    />
-                                    <h3 className="place">New York City</h3>
-                                </div>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card className="h-100 card-style">
-                                <div className="img-container">
-                                    <Card.Img
-                                        className="p-3 card-img"
-                                        variant="top"
-                                        src={img2}
-                                    />
-                                    <h3 className="place">Mont Blanc</h3>
-                                </div>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card className="h-100 card-style">
-                                <div className="img-container">
-                                    <Card.Img
-                                        className="p-3 card-img"
-                                        variant="top"
-                                        src={img3}
-                                    />
-                                    <h3 className="place">Norway Lake</h3>
-                                </div>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card className="h-100 card-style">
-                                <div className="img-container">
-                                    <Card.Img
-                                        className="p-3 card-img"
-                                        variant="top"
-                                        src={img4}
-                                    />
-                                    <h3 className="place">Affrican Park</h3>
-                                </div>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card className="h-100 card-style">
-                                <div className="img-container">
-                                    <Card.Img
-                                        className="p-3 card-img"
-                                        variant="top"
-                                        src={img5}
-                                    />
-                                    <h3 className="place">Vietnam</h3>
-                                </div>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card className="h-100 card-style">
-                                <div className="img-container">
-                                    <Card.Img
-                                        className="p-3 card-img"
-                                        variant="top"
-                                        src={img6}
-                                    />
-                                    <h3 className="place">Netherlands</h3>
-                                </div>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card className="h-100 card-style">
-                                <div className="img-container">
-                                    <Card.Img
-                                        className="p-3 card-img"
-                                        variant="top"
-                                        src={img7}
-                                    />
-                                    <h3 className="place">Greece</h3>
-                                </div>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card className="h-100 card-style">
-                                <div className="img-container">
-                                    <Card.Img
-                                        className="p-3 card-img"
-                                        variant="top"
-                                        src={img8}
-                                    />
-                                    <h3 className="place">Australia</h3>
-                                </div>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card className="h-100 card-style">
-                                <div className="img-container">
-                                    <Card.Img
-                                        className="p-3 card-img"
-                                        variant="top"
-                                        src={img9}
-                                    />
-                                    <h3 className="place">Costa Rica</h3>
-                                </div>
-                            </Card>
-                        </Col>
+                        {destinations.map((destination) => {
+                            return (
+                                <Col key={destination._id}>
+                                    <Card className="h-100 card-style">
+                                        <div className="img-container">
+                                            <Card.Img
+                                                className="p-3 card-img"
+                                                variant="top"
+                                                src={destination.image}
+                                            />
+                                            <h3 className="place">
+                                                {destination.name}
+                                            </h3>
+                                        </div>
+                                    </Card>
+                                </Col>
+                            );
+                        })}
                     </Row>
                 </div>
             </Container>
